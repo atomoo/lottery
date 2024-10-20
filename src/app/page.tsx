844dbd42882eb0e828867d15963fe27ff4be642f
atomoo <5802987+atomoo@users.noreply.github.com>
2024-10-20 16:20:46 +0800

    add tip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     return (
         <div className="h-screen font-[family-name:var(--font-geist-sans)]">
             <Lottery2DView />
+            {/* <div className="fixed bottom-0 right-0 text-gray-100">Data from L</div> */}
         </div>
     );
 }

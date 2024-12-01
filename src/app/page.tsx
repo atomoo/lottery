f3602d289e3b728d02ea91e5c37a4946ce74c6bd
atomoo <5802987+atomoo@users.noreply.github.com>
2024-12-01 22:06:56 +0800

    add thanks text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <div className="h-screen font-[family-name:var(--font-geist-sans)]">
             <Lottery2DView />
-            {/* <div className="fixed bottom-0 right-0 text-gray-100">Data from L</div> */}
+            <div className="fixed bottom-0 right-0 text-gray-50 opacity-90 select-none">Thanks L for the data.</div>
         </div>
     );
 }
